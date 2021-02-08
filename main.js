09e14f2aac90ca3959e42a79934dfd65a89a217a
Punnarat <punnarat.wat@dome.tu.ac.th>
2021-02-08 16:05:21 +0700

    Refactor code

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function sum(a, b){
     if(Array.isArray(a)){
         let total = 0
-        for(let i= 0; i<a.length ;i++){
-            total += a[i]
+        // for(let i= 0; i<a.length ;i++){
+        //     total += a[i]
             
-        }
+        // }
+        a.forEach((i)=>(total+=i))
         return total
     }
     return a+b
